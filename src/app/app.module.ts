a237b588b86e9e40402398095b734a5418be26cf
Sarath Bodepudi <sarathbabubodepudi@gmail.com>
2021-11-04 19:18:39 -0500

    angular modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './public/components/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
